bc474b78e308da519735984c1c591275e1da40ac
rustem.galiullin <rustem.galiullin@analog.io>
2025-04-14 15:44:33 +0400

    add avante plugin

--- nvim/lua/plugins/mason_lspconfig.lua
+++ nvim/lua/plugins/mason_lspconfig.lua
@@ -1,5 +1,26 @@
 return {
-  -- "williamboman/mason.nvim",
-  -- "williamboman/mason-lspconfig.nvim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
+
+  opts = {
+    setup = {
+      require("mason").setup(),
+      require("mason-lspconfig").setup(),
+      require("lspconfig").pyright.setup({
+        autostart = false,
+      }),
+      require("lspconfig").pylsp.setup({
+        settings = {
+          pylsp = {
+            plugins = {
+              rope_autoimport = {
+                enabled = true,
+              },
+            },
+          },
+        },
+      }),
+    },
+  },
 }
