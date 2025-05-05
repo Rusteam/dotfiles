b04d9a018339c4b95e0e56d508c970b0aeb1f0ad
rustem.galiullin <rustem.galiullin@analog.io>
2025-05-05 11:03:22 +0400

    add augment, rm avante, zen, jupynium

--- nvim/lua/plugins/zen.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/zen-mode.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-}
