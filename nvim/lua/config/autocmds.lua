b8375624691df739f66436b804340b3bd95bda6e
rustem.galiullin <rustem.galiullin@analog.io>
2026-06-15 20:46:31 +0400

    add dotenv file handler

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- .env files are detected as `dosini` (see config/options.lua); make sure they
+-- are never autoformatted or auto-wrapped.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dosini",
+  callback = function(args)
+    vim.b[args.buf].autoformat = false
+    vim.bo[args.buf].textwidth = 0
+    vim.bo[args.buf].formatoptions = vim.bo[args.buf].formatoptions:gsub("[tc]", "")
+  end,
+})
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.title = false
+
+-- nvim detects .env files as filetype `sh`, which makes shfmt / sh formatting
+-- split any unquoted line containing `;` onto multiple lines (`;` is a shell
+-- statement separator). Map them to `dosini` instead: no shell formatting, and
+-- it gives distinct key (dosiniLabel) / value (dosiniValue) / comment
+-- highlighting. Registered here so it wins the filetype race at startup.
+vim.filetype.add({
+  filename = {
+    [".env"] = "dosini",
+  },
+  pattern = {
+    ["%.env%.[%w_.-]+"] = "dosini", -- .env.local, .env.production, etc.
+    [".*%.env"] = "dosini", -- foo.env, anabot.env, etc.
+  },
+})
